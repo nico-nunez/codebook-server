7b665b82a386c59651e11b4c7cebdef3ddc18440
incogNic0 <incognico.dev@gmail.com>
2022-05-10 09:46:09 -0500

    minor bug fix

--- src/routes/tabs/tabs.validators.js
+++ src/routes/tabs/tabs.validators.js
@@ -22,7 +22,7 @@ const orderSchema = Joi.array()
 module.exports.tabSchema = Joi.object({
 	id: Joi.number(),
 	code_language: languageSchema,
-	content: Joi.string().trim(),
+	content: Joi.string().trim().allow(''),
 	cell_id: Joi.number(),
 });
 
